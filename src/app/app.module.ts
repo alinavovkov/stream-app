acf432120fd2dcbb2e8655ff62b7f4fda17e9cca
alinavovkov <alinvovkov@gmail.com>
2025-04-26 13:16:00 +0300

    the base structure of project was implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StreamComponent } from './pages/stream/stream.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StreamComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/stream/stream.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stream',
+  standalone: false,
+  templateUrl: './stream.component.html',
+  styleUrl: './stream.component.scss'
+})
+export class StreamComponent {
+
+}
